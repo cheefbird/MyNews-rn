9e5118945704fe9817f085e748607d2461c934a6
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-08 23:42:51 -0700

    Create basic ArticleList to replace TopNewsList.

---
+++ src/components/ArticleList.js
@@ -0,0 +1,11 @@
+// @flow
+
+import React, { Component } from 'react';
+import { FlatList } from 'react-native';
+import { connect } from 'react-redux';
+
+import ArticleCard from './ArticleCard';
+
+class ArticleList extends Component {}
+
+export default ArticleList;

01e36159a0526105189f7cefc88c4e66e9662125
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-08 23:30:44 -0700

    Cleanup TopNewsList screen.

--- src/screens/TopNewsList.js
+++ src/screens/TopNewsList.js
@@ -2,8 +2,7 @@
 
 import React, { Component } from 'react';
 
-import { Text, FlatList } from 'react-native';
-import { ListItem } from 'react-native-elements';
+import { FlatList } from 'react-native';
 
 import ArticleCard from '../components/ArticleCard';
 import staticData from '../helpers/staticData.json';
@@ -12,26 +11,24 @@ class TopNewsList extends Component {
   constructor(props) {
     super(props);
     const { articles } = staticData;
-    this.state = { newsArticles: articles };
+    this.state = { articles };
   }
 
   keyExtractor = (item, index) => item.title;
 
-  renderItem = ({ item }) => {
-    return (
-      <ArticleCard
-        title={item.title}
-        imageUri={item.urlToImage}
-        subtitle={item.description}
-        buttonText="See More"
-      />
-    );
-  };
+  renderItem = ({ item }) => (
+    <ArticleCard
+      title={item.title}
+      imageUri={item.urlToImage}
+      subtitle={item.description}
+      buttonText="See More"
+    />
+  );
 
   render() {
     return (
       <FlatList
-        data={this.state.newsArticles}
+        data={this.state.articles}
         extraData={this.state}
         keyExtractor={this.keyExtractor}
         renderItem={this.renderItem}

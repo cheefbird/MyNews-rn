9e4f3ea6cbcd30e4b5cdcb4dd624b4c7f29c47d8
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-14 23:20:07 -0700

    Create SearchActions.

---
+++ src/actions/SearchActions.js
@@ -0,0 +1,22 @@
+// @flow
+
+import { SEARCH_ARTICLES } from './types';
+import { API_TOKEN } from '../config/Network';
+
+export const searchArticles = (query) => {
+  const path = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_TOKEN}`;
+
+  return (dispatch) => {
+    fetch(path)
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.articles;
+
+        // TODO: extract this to its own action
+        dispatch({
+          type: SEARCH_ARTICLES,
+          payload: results,
+        });
+      });
+  };
+};
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,1 +1,4 @@
+// @flow
+
 export const FETCH_TOP_ARTICLES = 'fetch_top_articles';
+export const SEARCH_ARTICLES = 'search_articles';

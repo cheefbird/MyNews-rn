dbf354dc96f590a9e12216dc0d7546e2def18d5d
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-17 02:03:42 -0700

    Additional work on SearchResults screen.

--- src/screens/SearchResults.js
+++ src/screens/SearchResults.js
@@ -1,8 +1,25 @@
 // @flow
 
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { FlatList } from 'react-native';
+import { ListItem } from 'react-native-elements';
 
-class SearchResults extends Component {}
+class SearchResults extends Component {
+  keyExtractor = (item, index) => item.title;
+
+  renderItem = ({ item }) => (
+    <ListItem title={item.title} subtitle={item.description} />
+  );
+
+  render() {
+    return (
+      <FlatList
+        keyExtractor={this.keyExtractor}
+        data={this.props.articles}
+        renderItem={this.renderItem}
+      />
+    );
+  }
+}
 
 export default SearchResults;

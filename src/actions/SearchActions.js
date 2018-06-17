9e6767eb0d1b125eead840409d5f5361934fe42a
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-17 00:22:50 -0700

    Update search endpoint to use top-headlines and US.

--- src/actions/SearchActions.js
+++ src/actions/SearchActions.js
@@ -4,7 +4,7 @@ import { SEARCH_ARTICLES } from './types';
 import { API_TOKEN } from '../config/Network';
 
 export const searchArticles = (query) => {
-  const path = `https://newsapi.org/v2/everything?q=${query}&apiKey=${API_TOKEN}`;
+  const path = `https://newsapi.org/v2/top-headlines?q=${query}&country=us&apiKey=${API_TOKEN}`;
 
   return (dispatch) => {
     fetch(path)

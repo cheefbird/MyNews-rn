b9a770be0975b41825a9b88ff5088060b92e5885
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-18 12:58:10 -0700

    Handle incoming navigation params and show results in a simple ListItem.

--- src/screens/SearchResults.js
+++ src/screens/SearchResults.js
@@ -5,6 +5,13 @@ import { FlatList } from 'react-native';
 import { ListItem } from 'react-native-elements';
 
 class SearchResults extends Component {
+  constructor(props) {
+    super(props);
+    const articles = props.navigation.getParam('articles');
+
+    this.state = { articles };
+  }
+
   keyExtractor = (item, index) => item.title;
 
   renderItem = ({ item }) => (
@@ -15,7 +22,7 @@ class SearchResults extends Component {
     return (
       <FlatList
         keyExtractor={this.keyExtractor}
-        data={this.props.articles}
+        data={this.state.articles}
         renderItem={this.renderItem}
       />
     );

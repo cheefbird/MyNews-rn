7437e9f2cdc4c68898a30bcc6af1f0d490bbcbbd
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-19 14:34:21 -0700

    Update styling for the ArticleCard

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -7,15 +7,23 @@ import { Card, Button } from 'react-native-elements';
 export default class ArticleCard extends PureComponent {
   render() {
     const { title, urlToImage, description } = this.props.article;
+    const formattedTitle = title.toUpperCase();
+
+    let backupImage;
 
     return (
       <Card
-        containerStyle={{ padding: 8 }}
-        featuredTitle={title}
-        image={{ uri: urlToImage || 'https://placeimg.com/366/150/people.jpg' }}
+        // containerStyle={{ padding: 8 }}
+        image={{
+          uri: urlToImage || 'https://placeimg.com/366/150/people.jpg',
+        }}
+        imageStyle={{ backgroundColor: '#f4f4f4' }}
         imageProps={{ resizeMode: 'cover' }}
       >
-        <Text style={{ marginBottom: 10 }}>{description}</Text>
+        <Text style={{ fontSize: 18, fontWeight: 'bold', marginVertical: 10 }}>
+          {formattedTitle}
+        </Text>
+        <Text style={{ marginBottom: 10, fontSize: 12 }}>{description}</Text>
         <Button
           raised
           backgroundColor="#006bcd"
@@ -26,20 +34,3 @@ export default class ArticleCard extends PureComponent {
     );
   }
 }
-
-// const ArticleCard = (props) => {
-//   const { title, urlToImage, description } = props.article;
-
-//   return (
-//     <Card
-//       containerStyle={{ padding: 8 }}
-//       featuredTitle={title}
-//       // TODO: default image only loading sometimes??
-//       image={{ uri: urlToImage || 'https://placeimg.com/366/150/people' }}
-//       imageProps={{ resizeMode: 'cover' }}
-//     >
-//       <Text style={{ marginBottom: 10 }}>{description}</Text>
-//       <Button raised backgroundColor="#006bcd" title="Read More" />
-//     </Card>
-//   );
-// };

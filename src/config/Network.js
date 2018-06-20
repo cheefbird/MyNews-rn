ac021989fd514b81e8ae2239e6584bcd55a2c988
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-19 23:51:06 -0700

    Switch back to personal key. Update TopNews results number to 50.

--- src/config/Network.js
+++ src/config/Network.js
@@ -1,5 +1,5 @@
 // gmail
-// export const API_TOKEN = '01c6d185b0bb4b58942fd26b113f17e5';
+export const API_TOKEN = '01c6d185b0bb4b58942fd26b113f17e5';
 
 // work
-export const API_TOKEN = 'd139a2c3aae1404d806a6cd94c8d1bff';
+// export const API_TOKEN = 'd139a2c3aae1404d806a6cd94c8d1bff';

d57398102521caa37676d527b5a3ae767d940b3f
Francis Breidenbach <fbreidenbach@gmail.com>
2018-06-19 19:05:40 -0700

    Add empty description and image handling to ArticleCard. Add additional styling to article source text.

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -1,37 +1,54 @@
 // @flow
 
+import _ from 'lodash';
 import React, { PureComponent } from 'react';
-import { Text } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { Card, Button } from 'react-native-elements';
 
 export default class ArticleCard extends PureComponent {
+  setVisibility = (text) => {
+    if (text == null) {
+      return { visibility: 'hidden' };
+    }
+  };
+
   render() {
     const { title, urlToImage, description, source } = this.props.article;
-    const formattedTitle = title.toUpperCase();
+    const formattedSource = `Source: ${source.name.toUpperCase()}`;
+    const imageProtocol = _.truncate(urlToImage, {
+      separator: ':',
+      omission: '',
+    });
+
+    console.log(imageProtocol);
+
+    let descriptionText;
+    let imageUri;
+
+    if (description == null) {
+      descriptionText = null;
+    } else {
+      descriptionText = (
+        <Text style={styles.descriptionText}>{description}</Text>
+      );
+    }
+
+    if (imageProtocol === 'http') {
+      imageUri = 'https://placeimg.com/366/150/people.jpg';
+    } else {
+      imageUri = urlToImage;
+    }
 
     return (
       <Card
         // containerStyle={{ padding: 8 }}
-        image={{
-          uri: urlToImage || 'https://placeimg.com/366/150/people.jpg',
-        }}
-        imageStyle={{ backgroundColor: '#f4f4f4' }}
+        image={{ uri: imageUri }}
+        imageStyle={styles.image}
         imageProps={{ resizeMode: 'cover' }}
       >
-        <Text style={{ fontSize: 18, fontWeight: 'bold', marginVertical: 10 }}>
-          {formattedTitle}
-        </Text>
-        <Text
-          style={{
-            marginBottom: 8,
-            textAlign: 'center',
-            fontWeight: 'bold',
-            fontSize: 14,
-          }}
-        >
-          {source.name}
-        </Text>
-        <Text style={{ marginBottom: 10, fontSize: 12 }}>{description}</Text>
+        <Text style={styles.titleText}>{title.toUpperCase()}</Text>
+        <Text style={styles.sourceText}>{formattedSource}</Text>
+        {descriptionText}
         <Button
           raised
           backgroundColor="#006bcd"
@@ -42,3 +59,25 @@ export default class ArticleCard extends PureComponent {
     );
   }
 }
+
+const styles = StyleSheet.create({
+  image: {
+    backgroundColor: '#f4f4f4',
+  },
+  titleText: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginVertical: 10,
+  },
+  sourceText: {
+    marginBottom: 8,
+    textAlign: 'center',
+    fontWeight: 'bold',
+    fontSize: 16,
+    textDecorationLine: 'underline',
+  },
+  descriptionText: {
+    marginBottom: 10,
+    fontSize: 12,
+  },
+});
